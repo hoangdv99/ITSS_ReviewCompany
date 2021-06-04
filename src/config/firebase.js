b93c0b26963b8ff1a4446baf0db19048f93356a1
hoangdv99 <hoangdv257@gmail.com>
2021-06-05 00:01:33 +0700

    init project

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth';
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyAqcxQEQxDzcKEjYzhP5F2BWyPayQoWk4M",
+    authDomain: "review-company-efbb5.firebaseapp.com",
+    projectId: "review-company-efbb5",
+    storageBucket: "review-company-efbb5.appspot.com",
+    messagingSenderId: "480344273330",
+    appId: "1:480344273330:web:5a204620257f7da004f4ab"
+});
+
+export const auth = app.auth();
+export const firestore = app.firestore();
