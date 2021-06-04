2ff82777aefed7a9dae4c9f30b1c0d534eec594a
20176787 <huy.ln176787@sis.hust.edu.vn>
2021-06-05 01:35:15 +0700

    starter file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ListCompany from "./routes/listCompany/ListCompany";
+import RequestAddCompany from "./routes/requestAddCompany/RequestAddCompany";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          {/* example route */}
-          {/* <Route exact path="/login" component={Login} /> */}
-
+            <Route exact path="/companies" component={ListCompany} />
+            <Route exact path="/addCompany" component={RequestAddCompany} />
         </Switch>
       </Router>
     </div>
