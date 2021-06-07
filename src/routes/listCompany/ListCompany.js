a8fbd16c873604dc2b95b22039d0038b66aad56f
20176787 <huy.ln176787@sis.hust.edu.vn>
2021-06-07 17:57:35 +0700

    home design

--- src/routes/listCompany/ListCompany.js
+++ src/routes/listCompany/ListCompany.js
@@ -1,18 +1,116 @@
 import React from 'react';
-import CardCompany from "../../components/Company/CardCompany";
-const ListCompany = () => {
-    return(
-        <div>
-            <CardCompany/>
-            <CardCompany/>
-            <CardCompany/>
-            <CardCompany/>
-            <CardCompany/>
-            <CardCompany/>
-            <CardCompany/>
-            <CardCompany/>
-            <CardCompany/>
-        </div>
-    )
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Header from '../../components/Company/Header';
+import MainFeaturedPost from '../../components/Company/MainFeaturedPost';
+import FeaturedPost from '../../components/Company/FeaturedPost';
+import Main from '../../components/Company/Main';
+import Sidebar from '../../components/Company/Sidebar';
+import Footer from '../../components/Company/Footer';
+import Pagination from '@material-ui/lab/Pagination';
+const useStyles = makeStyles((theme) => ({
+    mainGrid: {
+        marginTop: theme.spacing(3),
+    },
+}));
+
+const sections = [
+    { title: 'Technology', url: '#' },
+    { title: 'Design', url: '#' },
+    { title: 'Culture', url: '#' },
+    { title: 'Business', url: '#' },
+    { title: 'Politics', url: '#' },
+    { title: 'Opinion', url: '#' },
+    { title: 'Science', url: '#' },
+    { title: 'Health', url: '#' },
+    { title: 'Style', url: '#' },
+    { title: 'Travel', url: '#' },
+];
+
+const mainFeaturedPost = {
+    title: 'Title of a longer featured blog post',
+    description:
+        "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    image: 'https://source.unsplash.com/random',
+    imgText: 'main image description',
+    linkText: 'Continue reading…',
+};
+
+const featuredPosts = [
+    {
+        title: 'Featured post',
+        date: 'Nov 12',
+        description:
+            'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: 'https://source.unsplash.com/random',
+        imageText: 'Image Text',
+    },
+    {
+        title: 'Post title',
+        date: 'Nov 11',
+        description:
+            'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: 'https://source.unsplash.com/random',
+        imageText: 'Image Text',
+    },
+];
+const sidebar = {
+    title: 'About',
+    description:
+        'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+    archives: [
+        { title: 'March 2020', url: '#' },
+        { title: 'February 2020', url: '#' },
+        { title: 'January 2020', url: '#' },
+        { title: 'November 1999', url: '#' },
+        { title: 'October 1999', url: '#' },
+        { title: 'September 1999', url: '#' },
+        { title: 'August 1999', url: '#' },
+        { title: 'July 1999', url: '#' },
+        { title: 'June 1999', url: '#' },
+        { title: 'May 1999', url: '#' },
+        { title: 'April 1999', url: '#' },
+    ],
+    social: [
+        { name: 'GitHub', icon: GitHubIcon },
+        { name: 'Twitter', icon: TwitterIcon },
+        { name: 'Facebook', icon: FacebookIcon },
+    ],
+};
+
+export default function ListCompany() {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <Header title="REVIEW COMPANY" sections={sections} />
+                <main>
+                    <MainFeaturedPost post={mainFeaturedPost} />
+                    <Grid container spacing={5} className={classes.mainGrid}>
+                        <Grid>
+                            <Pagination count={10} variant="outlined" shape="rounded" />
+                            {featuredPosts.map((post) => (
+                                <FeaturedPost key={post.title} post={post} />
+                            ))}
+                            <Pagination count={10} variant="outlined" shape="rounded" />
+                        </Grid>
+                        <Sidebar
+                            title={sidebar.title}
+                            description={sidebar.description}
+                            archives={sidebar.archives}
+                            social={sidebar.social}
+                        />
+                    </Grid>
+                </main>
+            </Container>
+            <Footer title="Team 2" description="From team 2 with love" />
+        </React.Fragment>
+    );
 }
-export default ListCompany;
